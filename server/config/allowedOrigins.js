af71b8262270b645ca270c2c02a3e159210f3cd6
Yashkalra12 <yash1578.be21@chitkara.edu.in>
2024-05-09 15:27:30 +0530

    server

---
+++ server/config/allowedOrigins.js
@@ -0,0 +1,6 @@
+const allowedOrigins = [
+  "https://recipen.vercel.app",
+  "https://recipen-backend.onrender.com",
+];
+
+module.exports = allowedOrigins;
---
+++ server/config/rolesList.js
@@ -0,0 +1,7 @@
+const ROLES_LIST = {
+  Admin: "Admin",
+  BasicUser: "BasicUser",
+  ProUser: "ProUser",
+};
+
+module.exports = ROLES_LIST;
